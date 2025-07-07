80b3de3eb25be74a609eb51f564a8b3eeb767f72
Daniel West <danthony03@gmail.com>
2025-07-07 08:32:44 -0400

    Third commit

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,44 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach((item) => {
+        const quantity = item.quantity;
+        const cost = item.cost;
+        total += parseFloat(item.cost.substring(1)) * quantity;
+    });
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1}));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1}));
+    } else {
+        dispatch(removeItem(item));
+    };
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let totalCost = 0;
+        totalCost += parseFloat(item.cost.substring(1)) * item.quantity;
+    return totalCost;
   };
 
   return (
